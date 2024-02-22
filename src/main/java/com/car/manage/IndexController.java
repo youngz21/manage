b0f2662ca99991da8d52b270a6e6616706945cf0
Woosung <youngz21@gmail.com>
2024-02-22 16:14:04 +0900

    dev branch code replace

--- src/main/java/com/car/manage/IndexController.java
+++ src/main/java/com/car/manage/IndexController.java
@@ -9,6 +9,8 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(){
+
+        System.out.println("dev 브랜치에서 추가한 코드");
         return "redirect:/car/management";
     }
 }
